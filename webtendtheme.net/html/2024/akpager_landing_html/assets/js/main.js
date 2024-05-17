fcfc6dbea35c52846a3a123ab8f4dce963601060
tanvirmadmax <tanvirmadmax@gmail.com>
2024-05-17 20:12:04 +0600

    first

---
+++ webtendtheme.net/html/2024/akpager_landing_html/assets/js/main.js
@@ -0,0 +1,177 @@
+(function ($) {
+
+    "use strict";
+
+    $(document).ready(function () {
+
+    // 01. Header Style and Scroll to Top
+        function headerStyle() {
+            if ($('.main-header').length) {
+                var windowpos = $(window).scrollTop();
+                var siteHeader = $('.main-header');
+                var scrollLink = $('.scroll-top');
+                if (windowpos >= 250) {
+                    siteHeader.addClass('fixed-header');
+                    scrollLink.fadeIn(300);
+                } else {
+                    siteHeader.removeClass('fixed-header');
+                    scrollLink.fadeOut(300);
+                }
+            }
+        }
+        headerStyle();
+        
+        
+        // 02. Dropdown menu
+        var mobileWidth = 992;
+        var navcollapse = $('.navigation li.dropdown');
+
+        navcollapse.hover(function () {
+            if ($(window).innerWidth() >= mobileWidth) {
+                $(this).children('ul').stop(true, false, true).slideToggle(300);
+                $(this).children('.megamenu').stop(true, false, true).slideToggle(300);
+            }
+        });
+        
+        // 03. Submenu Dropdown Toggle
+        if ($('.main-header .navigation li.dropdown ul').length) {
+            $('.main-header .navigation li.dropdown').append('<div class="dropdown-btn"><span class="fas fa-caret-down"></span></div>');
+
+            //Dropdown Button
+            $('.main-header .navigation li.dropdown .dropdown-btn').on('click', function () {
+                $(this).prev('ul').slideToggle(500);
+                $(this).prev('.megamenu').slideToggle(800);
+            });
+            
+            //Disable dropdown parent link
+            $('.navigation li.dropdown > a').on('click', function (e) {
+                e.preventDefault();
+            });
+        }
+        
+        //Submenu Dropdown Toggle
+        if ($('.main-header .main-menu').length) {
+            $('.main-header .main-menu .navbar-toggle').click(function () {
+                $(this).prev().prev().next().next().children('li.dropdown').hide();
+            });
+        }
+
+
+        //  hero prallax
+        var scene = document.getElementById('scene');
+        var parallaxInstance = new Parallax(scene);
+
+        
+        // Inner Pages Slider 
+        if ($('.inner-pages-slider').length) {
+            $('.inner-pages-slider').slick({
+                slidesToShow: 4,
+                slidesToScroll: 1,
+                infinite: true,
+                rows: 1,
+                speed: 400,
+                dots: true,
+                autoplay: true,
+                autoplaySpeed: 5000,
+                arrows: false,
+                responsive: [
+                    {
+                        breakpoint: 1200,
+                        settings: {
+                            slidesToShow: 3,
+                        }
+                    },
+                    {
+                        breakpoint: 767,
+                        settings: {
+                            slidesToShow: 2,
+                        }
+                    }
+                ]
+            });
+        } 
+        
+
+       // Scroll Down to Specific Area
+        $(".navigation a").on('click', function (e) {
+            e.preventDefault();
+            var hash = this.hash;
+            var position = $(hash).offset().top;
+            $("html").animate({
+                scrollTop: position
+            }, 2000);
+        });
+
+    
+        // 30. WOW Animation
+        if ($('.wow').length) {
+            var wow = new WOW({
+                boxClass: 'wow', // animated element css class (default is wow)
+                animateClass: 'animated', // animation css class (default is animated)
+                offset: 0, // distance to the element when triggering the animation (default is 0)
+                mobile: false, // trigger animations on mobile devices (default is true)
+                live: true // act on asynchronously loaded content (default is true)
+            });
+            wow.init();
+        }
+        
+        
+    });
+    
+    
+    /* ==========================================================================
+       When document is resize, do
+       ========================================================================== */
+
+    $(window).on('resize', function () {
+        var mobileWidth = 992;
+        var navcollapse = $('.navigation li.dropdown');
+        navcollapse.children('ul').hide();
+        navcollapse.children('.megamenu').hide();
+
+    });
+
+
+    /* ==========================================================================
+       When document is scroll, do
+       ========================================================================== */
+
+    $(window).on('scroll', function () {
+
+        // Header Style and Scroll to Top
+        function headerStyle() {
+            if ($('.main-header').length) {
+                var windowpos = $(window).scrollTop();
+                var siteHeader = $('.main-header');
+                var scrollLink = $('.scroll-top');
+                if (windowpos >= 100) {
+                    siteHeader.addClass('fixed-header');
+                    scrollLink.fadeIn(300);
+                } else {
+                    siteHeader.removeClass('fixed-header');
+                    scrollLink.fadeOut(300);
+                }
+            }
+        }
+        
+        headerStyle();
+
+    });
+
+    /* ==========================================================================
+       When document is loaded, do
+       ========================================================================== */
+
+    $(window).on('load', function () {
+
+        // 31. Preloader
+        function handlePreloader() {
+            if ($('.preloader').length) {
+                $('.preloader').delay(200).fadeOut(500);
+            }
+        }
+        handlePreloader();
+        
+    });
+
+})(window.jQuery);
